64c40b320848c13d9d6fea083dcd1d44a0f98c2c
wwerner <mail@wolfgang-werner.net>
2019-11-23 22:39:41 +0100

    Disable page reload when scrolling down

--- src/components/Meter.vue
+++ src/components/Meter.vue
@@ -8,6 +8,7 @@
       max="10"
       step="0.1"
       v-model="internalValue"
+      v-on:touchstart.native="convertToMousedown"
       @change="$emit('input', internalValue)"
     >
     </v-slider>
@@ -40,6 +41,22 @@ export default {
     valueColor() {
       return 'black';
     },
+    // we need this to be able to
+    // a) activate a range location
+    // while b) preventing the touchstart event from initiating a page reload by pulling down
+    convertToMousedown(e) {
+      const touches = e.changedTouches;
+      const first = touches[0];
+
+      const simulatedEvent = document.createEvent('MouseEvent');
+      simulatedEvent.initMouseEvent('click', true, true, window, 1,
+        first.screenX, first.screenY,
+        first.clientX, first.clientY, false,
+        false, false, false, 0, null);
+
+      first.target.dispatchEvent(simulatedEvent);
+      e.preventDefault();
+    },
   },
   mounted() {
     this.internalValue = this.value;
@@ -61,7 +78,7 @@ export default {
 }
 
 .v-slider--vertical {
-  height: 75vh;
+  height: 70vh;
   background: linear-gradient(to top, green, yellow 35%, red 75%);
 }
 
@@ -70,26 +87,26 @@ export default {
 }
 
 #meter
-  .v-slider--vertical .v-slider__track-container .primary {
+.v-slider--vertical .v-slider__track-container .primary {
   background: transparent !important;
   border-color: transparent !important;
 }
 
 #meter
-  .v-slider--vertical  .v-slider__track-container .v-slider__track-background {
+.v-slider--vertical .v-slider__track-container .v-slider__track-background {
   /* background: rgba(255,255,255,0.6) !important; */
   background: transparent !important;
   border-color: transparent !important;
 }
 
 #meter
-  .v-slider--vertical  .v-slider__track-container .v-slider__track-fill {
+.v-slider--vertical .v-slider__track-container .v-slider__track-fill {
   background: transparent !important;
   border-color: transparent !important;
 }
 
 #meter
-  .v-slider__thumb {
+.v-slider__thumb {
   position: absolute;
   width: 120px;
   height: 3em;
@@ -125,7 +142,7 @@ export default {
 }
 
 #meter
-  .v-slider__thumb-container--active .v-slider__thumb:before {
+.v-slider__thumb-container--active .v-slider__thumb:before {
   transform: scale(1) !important;
   top: 0;
 }
