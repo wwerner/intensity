0e805b27b7961b0185c749a17bc4b3a7fb9f2f68
Wolfgang Werner <mail@wolfgang-werner.net>
2019-12-04 07:25:37 +0100

    Add FAB menu

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
-  <v-app id="app">
-    <router-view/>
-    <div id="nav">
-      <router-link to="/">Intensität</router-link> |
-      <router-link to="/settings">Einstellungen</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <v-app id="app" style="width: 100%">
+    <v-container fluid>
+      <v-row class="child-flex">
+        <v-col cols="12">
+          <router-view/>
+
+          <v-speed-dial
+            v-model="menu"
+            bottom absolute right
+            direction="top"
+            transition="slide-y-reverse-transition"
+          >
+            <template v-slot:activator>
+              <v-btn
+                color="blue darken-2"
+                dark
+                fab
+              >
+                <v-icon v-if="menu">mdi-close</v-icon>
+                <v-icon v-else>mdi-menu</v-icon>
+              </v-btn>
+            </template>
+            <v-btn
+              fab
+              dark
+              small
+              color="green"
+              to="/about"
+            >
+              <v-icon>mdi-chart-bell-curve</v-icon>
+            </v-btn>
+            <v-btn
+              fab
+              dark
+              small
+              color="yellow"
+              to="/about"
+            >
+              <v-icon>mdi-information-outline</v-icon>
+            </v-btn>
+            <v-btn
+              fab
+              dark
+              small
+              color="orange"
+              to="/settings"
+            >
+              <v-icon>mdi-settings</v-icon>
+            </v-btn>
+            <v-btn
+              fab
+              dark
+              small
+              color="red"
+              to="/"
+            >
+              <v-icon>mdi-thermometer</v-icon>
+            </v-btn>
+          </v-speed-dial>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      menu: false,
+    };
+  },
+};
+</script>
 
 <style>
-#app {
-  font-family: 'Roboto', sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  #app {
+    font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav {
-  text-align: right;
-  margin-right: 1em;
-  position: absolute;
-  bottom: 0.5em;
-  right: 0.2em;
-}
 </style>
