2f3f711e9f62e51508f5f723bc4154c37ef6a121
Wolfgang Werner <mail@wolfgang-werner.net>
2019-12-21 15:40:56 +0100

    Update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Über Intensity</h1>
+    <h2>Anwendungsbereich</h2>
+    <h2>Datenschutz</h2>
+    <p>
+      <b>Intensity</b> verwendet keine Cookies und sammelt keinerlei Daten über den Benutzer.
+      Der Verlauf wird ausschließlich auf dem Gerät des Benutzers gespeichert.
+    </p>
+
+    <h2>Technologie</h2>
+    <v-btn
+      link
+      href="https://github.com/wwerner/intensity"
+    >
+      <v-icon>mdi-github-circle</v-icon>
+      Source Code
+    </v-btn>
   </div>
 </template>
